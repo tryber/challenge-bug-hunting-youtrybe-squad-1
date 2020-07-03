5aa87994c14974334a0ca1d9ca184a96e5e46625
Hfreitas <hfreitas86@gmail.com>
2020-07-03 14:37:16 -0300

    criado endpoint para busca de videos relacionados

--- src/api/service.js
+++ src/api/service.js
@@ -1,8 +1,9 @@
 const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3';
 const YOUTUBE_AUTH_KEY = () => {
-  const auth = process.env.NODE_ENV === 'development'
-    ? process.env.REACT_APP_API_KEY
-    : null;
+  const auth =
+    process.env.NODE_ENV === 'development'
+      ? process.env.REACT_APP_API_KEY
+      : null;
   return auth;
 };
 
@@ -34,6 +35,19 @@ export const getVideoInfo = async (videoId) => {
 export const getVideoComments = async (videoId) => {
   const urlParams = `part=snippet&videoId=${videoId}&textFormat=plainText&key=${YOUTUBE_AUTH_KEY()}`;
   const URL = `${YOUTUBE_API_URL}/commentThreads?${urlParams}`;
+
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getRelatedVideos = async (videoId) => {
+  const urlParams = `part=snippet&relatedToVideoId=${videoId}&type=video&key=${YOUTUBE_AUTH_KEY()}`;
+  const URL = `${YOUTUBE_API_URL}/search?${urlParams}`;
 
   try {
     const response = await fetch(URL);
